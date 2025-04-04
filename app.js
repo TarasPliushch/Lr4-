0ae8d7af784a2c02ad671c5c309a0aa2d8964e7e
TarasPliushch <pliushch_ak23@nuwm.edu.ua>
2025-04-04 15:06:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,62 @@
-// ... (попередній код setup)
+const { createApp, ref, computed } = Vue;
+
+createApp({
+  setup() {
+    const newTodo = ref('');
+    const todos = ref([]);
+    const editingId = ref(null);
+    const editedTodo = ref('');
+
+    const addTodo = () => {
+      if (newTodo.value.trim()) {
+        todos.value.push({
+          id: Date.now(),
+          text: newTodo.value.trim(),
+          completed: false
+        });
+        newTodo.value = '';
+      }
+    };
+
+    const removeTodo = (id) => {
+      todos.value = todos.value.filter(todo => todo.id !== id);
+    };
+
+    const startEditing = (todo) => {
+      editingId.value = todo.id;
+      editedTodo.value = todo.text;
+    };
+
+    const finishEditing = (id) => {
+      const todo = todos.value.find(t => t.id === id);
+      if (todo) {
+        todo.text = editedTodo.value;
+      }
+      editingId.value = null;
+    };
+
+    const activeTodos = computed(() => {
+      return todos.value.filter(todo => !todo.completed);
+    });
+
+    const completedTodos = computed(() => {
+      return todos.value.filter(todo => todo.completed);
+    });
+
+    return {
+      newTodo,
+      todos,
+      addTodo,
+      removeTodo,
+      editingId,
+      editedTodo,
+      startEditing,
+      finishEditing,
+      activeTodos,
+      completedTodos
+    };
+  }
+}).mount('#app');
 
 template: `
   <h1>Todo App</h1>
